701127da28514624f067c686066d467fb4bf7a5b
Amwighio <159983597+manan7866@users.noreply.github.com>
2024-06-11 18:15:46 -0700

    Coundown Timer

---
+++ index.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+import {differenceInSeconds} from "date-fns";
+
+
+let time = await inquirer.prompt({
+    name : "UserInput",
+    type : "number",
+    message : "Start CountDown",
+    validate : (input)=> {
+        if (isNaN(input)){
+            return "Please Enter vaild Number"
+        }else {
+            return true
+        }
+    }
+})
+
+let input = time.UserInput
+
+function starttime(val : number){
+    const intime = new Date().setSeconds(new Date().getSeconds()+ val)
+    const intervalTime = new Date(intime)
+    setInterval((()=>{
+        const currtime = new  Date()
+        const timediff = differenceInSeconds(intervalTime, currtime)
+        if (timediff <=0){
+            console.log("Time ' out")
+            process.exit()
+        }
+        const min = Math.floor((timediff%(3600*24))/3600)
+        const  sec = Math.floor(timediff%60)
+        console.log(`${min.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`)
+    }),900)
+}
+starttime(input)
+
